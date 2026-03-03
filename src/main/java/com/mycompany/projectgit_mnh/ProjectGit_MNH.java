f89bce58b9dc5ac465c52413c06b847b9c24a571
YesaelSH <Pomuch20@DESKTOP-S1JQQ8F>
2026-03-03 07:37:55 -0600

    feat:estructura inicial del proyecto.

---
+++ src/main/java/com/mycompany/projectgit_mnh/ProjectGit_MNH.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectgit_mnh;
+
+/**
+ *
+ * @author Pomuch20
+ */
+public class ProjectGit_MNH {
+
+    public static void main(String[] args) {
+        System.out.println("--- Sistema de Gestión");
+    }
+}
