02e999e6452d4e11caf27b3e793ac7f6322b8bf1
YesaelSH <Pomuch20@DESKTOP-S1JQQ8F>
2026-03-09 07:45:38 -0600

    feat:estructura inicial del proyecto.

--- src/main/java/com/mycompany/projectgit_mnh/ProjectGit_MNH.java
+++ src/main/java/com/mycompany/projectgit_mnh/ProjectGit_MNH.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.projectgit_mnh;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Pomuch20
@@ -13,6 +15,48 @@ public class ProjectGit_MNH {
     public static void main(String[] args) {
         System.out.println("--- Sistema de Gestión");
         System.out.println("Bienvenido, Usuario Estudiante.");
+        Scanner cin = new Scanner (System.in);// declaracion de variable para la lectura de los datos de entrada 
+        
+        //Declarar variables 
+        int n1,n2, suma;
+        
+        
+        System.out.print("Introduce primer número:");
+        n1 = cin.nextInt(); //El valor que introduce el usuario es asignado a la variablr n1
+        
+        System.out.print("introduce segundo número:");
+        n2 = cin.nextInt(); // El valor que introduce el usuario es asignado a la variable n2
+        
+        //Proceso
+        suma = n1 + n2;
+        
+        //Salida 
+        System.out.println("El resultado es: " + suma);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         
     }
